e3ec866d8a2ff973b3fcd6d873b733fe2e452a2b
Jay <73510469+spanosjustin@users.noreply.github.com>
2024-08-06 16:50:07 -0400

    Update App.js
    Removing test code I meant to before last commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,6 @@ const Z_CLIENT_SECRET = process.env.REACT_APP_ZOOM_CLIENT_SECRET;
 const Z_AUTH_URL = process.env.REACT_APP_ZOOM_AUTH_URL;
 const Z_BASE_URL = process.env.REACT_APP_ZOOM_API_BASE_URL;
 
-/*
-const Z_ACCOUNT_ID = 'jndL9khkRrOWgCa9ODk2CA';
-const Z_CLIENT_ID = 'wXjzQC4NTPSOYiJqoo1D0A';
-const Z_CLIENT_SECRET = 'a2kney7NBX5ihiPOEzkQEr5sX1TUvARv';
-const Z_AUTH_URL = '/oauth/token';
-const Z_BASE_URL = '/v2';
-*/
 const webinarPrice = 12.99;
 
 // accessing zoom
